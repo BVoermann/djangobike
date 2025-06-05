26fa06449e73e18d5236d3bbc1b41b84b4603d36
BVoermann <Benjamin-Voermann@web.de>
2025-06-05 14:14:19 +0200

    adds gitignore ; changes the urls for better routing

--- djangobike/urls.py
+++ djangobike/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bikeshop.urls')),
     path('procurement/', include('procurement.urls')),
     path('production/', include('production.urls')),
